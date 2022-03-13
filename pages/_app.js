136c7c717b8ccf6ba6d0df97d79ee269d99265c3
Gino Stoian <ginostoian@gmail.com>
2022-03-13 02:07:59 +0200

    create firebase auth and connect db

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { AuthProvider } from '@/lib/auth'
+import { CSSReset, ThemeProvider } from '@chakra-ui/core'
+import { Global, css } from '@emotion/core';
+import theme from '@/styles/theme'
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
